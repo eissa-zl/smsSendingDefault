d1f2ef34fd2dedbbbdf0218cc069f7fe6e3bef50
Eissa Faheem <eissa.faheem@ziroh.com>
2023-11-28 11:34:32 +0530

    lol

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Button, Linking, Platform, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  async function fun() {
+    let phoneNumber = '7905201778';
+    let message = 'Hellow how are you Eissa?';
+    const separator = Platform.OS === 'ios' ? '&' : '?';
+    const url = `sms:${phoneNumber}${separator}body=${message}`;
+    await Linking.openURL(url);
+  }
+
+  return (
+    <>
+      <View style={{padding: 100}}>
+        <Button title="Invite" onPress={() => fun()} />
+      </View>
+    </>
+  );
+}
+
+export default App;
